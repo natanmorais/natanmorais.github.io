9b75bcc6b6bbb2f2a074b3eb1cb2f23fe009f73e
natanmorais <morais_natan@hotmail.com>
2017-07-12 15:48:25 -0300

    Mudanças na div principal
    #Desktop: Div se mantém no local mesmo com resize
    #Mobile: Tentativa de inserir banner

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,9 @@
 function exibeBanner() {
-    if (screen.width > 639) {
+    if (screen.width > 768) {
         document.write('<video id="video" poster="media/static_banner.png" id="bgvid" playsinline autoplay muted loop><source src="media/banner_keyboard2.mp4" type="video/mp4"></video>');
         return;
     } else {
-        document.write('Tela pequena!');
+        document.write('<img src=media/static_banner.png \>');
         return;
     }
 }
