daa92b1872e288d965745f16e5eec25e21b41d80
natanmorais <morais_natan@hotmail.com>
2017-07-12 17:58:21 -0300

    Resolvido Banner Inicial

--- js/utils.js
+++ js/utils.js
@@ -3,7 +3,7 @@ function exibeBanner() {
         document.write('<video id="video" poster="media/static_banner.png" id="bgvid" playsinline autoplay muted loop><source src="media/banner_keyboard2.mp4" type="video/mp4"></video>');
         return;
     } else {
-        document.write('<img src=media/static_banner.png \>');
+        //document.write('<img src=media/static_banner.png \>');
         return;
     }
 }
