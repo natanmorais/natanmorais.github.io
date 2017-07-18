378e45db8714f01a86be56b981f4c295cb0c22af
natanmorais <morais_natan@hotmail.com>
2017-07-18 16:03:41 -0300

    Arrumado Esqueleto do Site

--- js/utils.js
+++ js/utils.js
@@ -3,7 +3,15 @@ function exibeBanner() {
         document.write('<video id="video" poster="media/static_banner.png" id="bgvid" playsinline autoplay muted loop><source src="media/banner_keyboard2.mp4" type="video/mp4"></video>');
         return;
     } else {
-        //document.write('<img src=media/static_banner.png \>');
         return;
+    }
+}
+
+function showEmail() {
+    var x = document.getElementById('email');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
     }
 }
